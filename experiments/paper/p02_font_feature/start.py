07b391e79eb2658bf5ce329555f253e80b39cdf8
38dunn@gmail.com <Dann38>
2026-07-22 18:12:39 +0800

    fix #

--- experiments/paper/p02_font_feature/start.py
+++ experiments/paper/p02_font_feature/start.py
@@ -30,14 +30,14 @@ if __name__ == '__main__':
 
     configs = {}
     for seed in range(3):
-        # configs[f"font_emb_seed_{seed}"] = {
-        #     **default_arch(input_dim=527, epochs=EPOCHS, early_stopping_patience=3, seed=seed),
-        #     "_cache_dir": "tmp_feature_font_emb",
-        # }
-        # configs[f"font_emb_16_seed_{seed}"] = {
-        #     **default_arch(input_dim=31, epochs=EPOCHS, early_stopping_patience=3, seed=seed),
-        #     "_cache_dir": "tmp_feature_font_emb_16",
-        # }
+        configs[f"font_emb_seed_{seed}"] = {
+            **default_arch(input_dim=527, epochs=EPOCHS, early_stopping_patience=3, seed=seed),
+            "_cache_dir": "tmp_feature_font_emb",
+        }
+        configs[f"font_emb_16_seed_{seed}"] = {
+            **default_arch(input_dim=31, epochs=EPOCHS, early_stopping_patience=3, seed=seed),
+            "_cache_dir": "tmp_feature_font_emb_16",
+        }
         configs[f"font_emb_32_seed_{seed}"] = {
             **default_arch(input_dim=21+32, epochs=EPOCHS, early_stopping_patience=3, seed=seed),
             "_cache_dir": "tmp_feature_font_emb_32",
